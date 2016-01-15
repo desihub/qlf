41bdd90aa394b35412f62e5de512f67c46250a82
Alisson Lanot <alisson.lanot@linea.gov.br>
2016-01-15 13:14:25 -0200

    updated mkconfig.py

--- mkconfig.py
+++ mkconfig.py
@@ -1,9 +1,24 @@
-import yaml
 import json
+import yaml
 
 with open('config/ql.json', 'r') as fp:
     data = json.load(fp)
 
-with open('config/ql.yaml', 'w') as fp2:
-    yaml.safe_dump(data,fp2)
+root = {"PipeLine":[]}
 
+for item in data["tasks"]:
+    steps = []
+    for item2 in item["steps"]:
+        step = {}
+        step["ModuleName"] = item2["source"]
+        step["Name"] = item2["name"]
+        config = {} 
+        for item3 in item2["configuration"]:
+            config[item3["label"]] = item3["default"]
+        step["kwargs"] = config
+        steps.append(step)
+    root["PipeLine"].append({"StepName":item["name"],"Steps":steps})
+
+print root
+with open("config/ql.yaml","w") as fp:
+    yaml.safe_dump(root,fp)
