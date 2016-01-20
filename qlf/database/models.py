8b169084d09e01421c3c5a87983f04ded92f43bc
Alisson Lanot <alisson.lanot@linea.gov.br>
2016-01-20 16:59:29 -0200

    added django folder

---
+++ qlf/database/models.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Exposure(models.Model):
+    exposure_id = models.AutoField(primary_key=True)
+    prop1 = models.IntegerField(default=0)
+    prop2 = models.FloatField(default=0.0)
+
+class Spectrograph(models.Model):
+    spectrograph_id = models.AutoField(primary_key=True)
+    prop1 = models.IntegerField(default=0)
+    prop2 = models.FloatField(default=0.0)
+
+class CCD(models.Model):
+    ccd_id = models.AutoField(primary_key=True)
+    exposure_id = models.ForeignKey(Exposure, on_delete=models.CASCADE)
+    spectrograph_id = models.ForeignKey(Spectrograph, on_delete=models.CASCADE)
+    arm = models.CharField(max_length=200)
+    prop1 = models.IntegerField(default=0)
+    prop2 = models.FloatField(default=0.0)
+
+class Spectra(models.Model):
+    spectra_id = models.AutoField(primary_key=True)
+    ccd_id = models.ForeignKey(CCD, on_delete=models.CASCADE)
+    counts = models.IntegerField(default=0)
+    sn_ratio = models.FloatField(default=0.0)
+    prop1 = models.FloatField(default=0.0)
+    prop2 = models.FloatField(default=0.0)
+
+class Fiber(models.Model):
+    fiber_id = models.AutoField(primary_key=True)
+    spectrograph_id = models.ForeignKey(Spectrograph, on_delete=models.CASCADE)
+    prop1 = models.IntegerField(default=0)
+    prop2 = models.FloatField(default=0.0)
