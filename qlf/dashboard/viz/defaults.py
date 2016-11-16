88c5dcd14ffac6748758955e74ed444df741b912
Angelo Fausti <afausti@lsst.org>
2016-11-16 09:50:29 -0700

    Added initial  layout for the SNR vs. mag plot

--- qlf/dashboard/viz/defaults.py
+++ qlf/dashboard/viz/defaults.py
@@ -11,7 +11,6 @@ def init_time_series_plot(hover):
     plot = Figure(
         plot_height=300,
         plot_width=900,
-        responsive=True,
         tools="xpan,xwheel_zoom,xbox_zoom,reset,tap",
         x_axis_type='datetime',
         min_border_top=0,
@@ -31,6 +30,28 @@ def init_time_series_plot(hover):
         months=["%d %B"],
         years=["%d %B"],
     ))
+    return plot
+
+
+def init_xy_plot(hover):
+    """
+    Defaults for xy plots
+    """
+    plot = Figure(
+        plot_height=200,
+        plot_width=300,
+        tools="pan,wheel_zoom,box_zoom,reset",
+        min_border_top=0,
+        min_border_right=10,
+        min_border_bottom=50,
+        min_border_left=50
+    )
+    plot.add_tools(hover)
+    plot.xaxis.axis_label_text_font_style = "normal"
+    plot.xaxis.axis_label_text_font_size = "12pt"
+    plot.yaxis.axis_label_text_font_style = "normal"
+    plot.yaxis.axis_label_text_font_size = "12pt"
+
     return plot
 
 
