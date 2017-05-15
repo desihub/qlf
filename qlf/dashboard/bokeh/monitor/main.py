eb423d72b1fe695a216cc52b4248e4ed7052deeb
rafaelbritoloureiro <rafael56@hotmail.com>
2017-05-15 15:24:20 -0300

    Implementacao Monitor: Display processing steps in the bar chart

--- qlf/dashboard/bokeh/monitor/main.py
+++ qlf/dashboard/bokeh/monitor/main.py
@@ -1,50 +1,41 @@
 from bokeh.plotting import figure, output_file, curdoc
 from bokeh.models import ColumnDataSource, LabelSet, Label
 from bokeh.driving import count
-import random
 
-
-bars_list = list()
+bars = list()
 label_name = list()
-labels_dict = dict()
-listColor=list()
+cameras = dict()
+listColor = list()
 
 # AF: height of the bars in the bokeh chart
-heightList=list()
+barsHeight = list()
 
-
-
-for i in range(30):
-    # AF: not used below
-    n = random.randint(1,7)
-    listColor.append('#0000FF')
-    heightList.append(0.5)
-    
-for i in range(30):
+for num in range(30):
     # AF: add bars
-    bars_list.append(i)
-   
-    # AF: add labels for camera and processing steps
-    
+    bars.append(num)
+    listColor.append('#0000FF')
+    barsHeight.append(0.5)
+    # AF: add labels for cameras and processing steps
+
     if 'z9' not in label_name:
-        labels_dict['z' + str(i)] = Label(x=-6.5, y = i -.3, text='z' + str(i))
-        labels_dict['stagez' + str(i)] = Label(x=50, y = i -.3, text='start ' + str(i), background_fill_color='white', background_fill_alpha=0.7)
-        label_name.append('z' + str(i))
+        cameras['z' + str(num)] = Label(x=-6.5, y=num - .3, text='z' + str(num))
+        cameras['stagez' + str(num)] = Label(x=50, y=num - .3, text='Initializing ', background_fill_color='white',
+                                             background_fill_alpha=0.7)
+        label_name.append('z' + str(num))
     elif 'r9' not in label_name:
-        labels_dict['r' + str(i - 10)] = Label(x=-6.5, y = i -.3, text='r' + str(i - 10))
-        labels_dict['stager' + str(i - 10)] = Label(x=50, y = i -.3, text='start ' + str(i), background_fill_color='white', background_fill_alpha=0.7)
-        label_name.append('r' + str(i - 10))
+        cameras['r' + str(num - 10)] = Label(x=-6.5, y=num - .3, text='r' + str(num - 10))
+        cameras['stager' + str(num - 10)] = Label(x=50, y=num - .3, text='Initializing ',
+                                                  background_fill_color='white', background_fill_alpha=0.7)
+        label_name.append('r' + str(num - 10))
     elif 'g9' not in label_name:
-        labels_dict['g' + str(i - 20)] = Label(x=-6.5, y = i -.3, text='g' + str(i - 20))
-        labels_dict['stageg' + str(i - 20)] = Label(x=50, y = i -.3, text='start ' + str(i), render_mode='css', background_fill_color='white', background_fill_alpha=0.7)
-        label_name.append('g' + str(i - 20))
+        cameras['g' + str(num - 20)] = Label(x=-6.5, y=num - .3, text='g' + str(num - 20))
+        cameras['stageg' + str(num - 20)] = Label(x=50, y=num - .3, text='Initializing ', render_mode='css',
+                                                  background_fill_color='white', background_fill_alpha=0.7)
+        label_name.append('g' + str(num - 20))
 
-# AF: y is not being used anywhere
-y = bars_list
-
-plot = figure(title="Monitor", height=900, x_range=(-9, 120))
-for i in labels_dict:
-    plot.add_layout(labels_dict[i]) 
+plot = figure(height=900, x_range=(-9, 120))
+for cam in cameras:
+    plot.add_layout(cameras[cam])
 
 # AF: Move plot style configuration to theme.yaml
 plot.xaxis.visible = False
@@ -54,48 +45,54 @@ sourceBar = ColumnDataSource(dict(y=[0], right=[0], height=[0], color=['#0000FF'
 plot.hbar(y='y', right='right', height='height', color='color', source=sourceBar)
 curdoc().add_root(plot)
 
+
 @count()
 def update(t):
-    lista = []
-    # AF: not being used
-    urls_dict = dict()
-    
-    # AF: ?
-    name = 'Running'
-    
-    for i in range(30):
-        lista.append(0)
-        
-    try:
-        
-        # AF: loop over cameras 
-        for i in labels_dict:
-            if i[:5] != 'stage':            
-                # AF: add example of log files in qlf/test/logs and use a relative path here
-                arq = open('/home/rafael/qlf-17/qlf/dashboard/bokeh/monitor/teste_log/'+i+'.log', 'r')
+    barsRight = list()
+
+    for num in range(30):
+        barsRight.append(0)
+
+    # AF: loop over camerass
+    for cam in cameras:
+        if cam[:5] != 'stage':
+            log = list()
+            try:
+                arq = open('../test/log/' + cam + '.log', 'r')
                 log = arq.readlines()
-                if i[:1] == 'z':
-                    lista[int(i[1:])] = len(log)
-                if i[:1] == 'r':
-                    lista[int(i[1:]) - 20] = len(log)
-                if i[:1] == 'g':
-                    lista[int(i[1:]) - 10] = len(log)
-                
-                # AF: currrent line
-                stage = log[len(log) -1]
-                
-                if 'QuickLook INFO' in stage:
-                    
-                    # AF: replace this by a search for the processing steps defined in the design
-                    if stage.split(':')[3].split()[0] == 'Running' or stage.split(':')[3].split()[0] == 'Pipeline':
-                        name = stage.split(':')[3].split()[0] +' '+ stage.split(':')[3].split()[1]
-                        labels_dict['stage'+ i].text = name
-                        
-    except Exception as e:
-        for i in labels_dict:
-            if i[:5] != 'stage':
-                labels_dict['stage'+ i].text = 'Log not found'
-    new_datat = dict(y=y, right=lista, height=heightList, color=listColor)
+            except Exception as e:
+                e
+            if cam[:1] == 'z':
+                barsRight[int(cam[1:])] = len(log)
+            if cam[:1] == 'r':
+                barsRight[int(cam[1:]) - 20] = len(log)
+            if cam[:1] == 'g':
+                barsRight[int(cam[1:]) - 10] = len(log)
+
+            # AF: currrent line
+
+            for line in log[::-1]:
+                if 'Pipeline completed' in line:
+                    cameras['stage' + cam].text = 'Pipeline completed'
+                    break
+                elif 'Sky Subtraction' in line:
+                    cameras['stage' + cam].text = 'Sky Subtraction'
+                    break
+                elif 'Boxcar Extraction' in line:
+                    cameras['stage' + cam].text = 'Boxcar Extraction'
+                    break
+                elif 'Preprocessing' in line:
+                    cameras['stage' + cam].text = 'Preprocessing'
+                    break
+                else:
+                    cameras['stage' + cam].text = 'Initializing'
+                    break
+
+
+
+
+    new_datat = dict(y=bars, right=barsRight, height=barsHeight, color=listColor)
     sourceBar.stream(new_datat, 30)
+
 
 curdoc().add_periodic_callback(update, 100)
