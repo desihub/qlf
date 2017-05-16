612ca9386567bf5105514ecdee43bd81a6f243e5
Angelo Fausti Neto <angelofausti@desidev.lbl.gov>
2017-05-16 14:51:10 -0700

    Fixed pipeline log name

--- bin/qlf_pipeline.py
+++ bin/qlf_pipeline.py
@@ -176,16 +176,16 @@ class QLFPipeline(object):
         with subprocess.Popen(cmd, stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE, shell=True, cwd=cwd) as process:
             for line in iter(process.stdout.readline, bytes()):
-                logfile.write(line)
-                logfile.flush()
+                logname.write(line)
+                logname.flush()
 
             for line in iter(process.stderr.readline, bytes()):
-                logfile.write(line)
-                logfile.flush()
+                logname.write(line)
+                logname.flush()
 
             retcode = process.wait()
 
-        logfile.close()
+        logname.close()
 
         camera['end'] = str(datetime.datetime.now())
         camera['status'] = 0
