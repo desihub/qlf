4d2b0b840461ba2efbfcdf7e23fcf7507eecdb0e
Angelo Fausti <afausti@lsst.org>
2017-05-28 23:24:06 -0700

    Removed db initialization password in run.sh script, improved messages

--- qlf/run.sh
+++ qlf/run.sh
@@ -7,17 +7,18 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 if [ "$QLF_ROOT" == "" ];
 then
-	echo "Set QLF_ROOT environment variable first. Example:"
+	echo "Set QLF_ROOT environment variable first. In a default QLF installation, you should run:"
 	echo "export QLF_ROOT=$HOME/quicklook"
 	exit 1
 fi
-echo "Setting DESI Quick Look environment..."
 
-for package in desispec desiutil; do
-	echo "Setting $package..."
-	export PATH=$QLF_ROOT/$package/bin:$PATH
-	export PYTHONPATH=$QLF_ROOT/$package/py:$PYTHONPATH
-done
+
+if [ "$(conda info -e | grep "*" | cut -d " " -f1)" != "quicklook" ];
+then
+    echo "Set quicklook conda environment first. In a default QLF installation, you should run:"
+    echo "source ~/miniconda3/bin/activate quicklook"
+    exit 1
+fi
 
 echo "Initializing QLF database..."
 # Test user for the development db
@@ -32,31 +33,34 @@ then
     rm $DEVDB
 fi
 
-python -Wi manage.py migrate
+python -Wi manage.py migrate > /dev/null
 
-# Create django superuser
-# The password created here is used to access de admin interface and the browsable API
-echo
-echo "For development you might use a password like: $TEST_USER_PASSWD"
-echo
-
-python -Wi manage.py createsuperuser --username $TEST_USER --email $TEST_USER_EMAIL
+python -Wi manage.py createsuperuser --noinput --username $TEST_USER --email $TEST_USER_EMAIL
 
 # Start QLF web application
 
 if [ -f $QLF_ROOT/run.pgid ]; then
     RUN_PGID=`cat $QLF_ROOT/run.pgid`
-    ps opgid | grep $RUN_PGID > /dev/null && echo "QLF is running, terminating..."; kill -- -$RUN_PGID > /dev/null
+    ps opgid | grep $RUN_PGID > /dev/null && echo "Another instance of QLF is running, terminating..."; kill -- -$RUN_PGID > /dev/null
 fi
 
-# Start the servers and save the PGID, django and bokeh share the same PGID
+# Start django and bokeh servers and save the process group id
 
 echo "Starting QLF..."
+
+echo "Setting DESI Quick Look environment..."
+
+for package in desispec desiutil; do
+	echo "Setting $package..."
+	export PATH=$QLF_ROOT/$package/bin:$PATH
+	export PYTHONPATH=$QLF_ROOT/$package/py:$PYTHONPATH
+done
+
 nohup python -Wi manage.py runserver &> $QLF_ROOT/runserver.log & echo $(ps opgid= $!) > $QLF_ROOT/run.pgid
 nohup bokeh serve --allow-websocket-origin=localhost:8000 dashboard/bokeh/qasnr dashboard/bokeh/monitor dashboard/bokeh/exposures &> $QLF_ROOT/bokeh.log & 
 
 echo "QLF web application is available at http://localhost:8000"
-echo "QLF is processing, watch qlf.log"
+echo "QLF daemon started Quick Look, watch qlf.log..."
 
-# QLF daemon
+# Start QLF daemon
 python -Wi ../bin/qlf_daemon.py
