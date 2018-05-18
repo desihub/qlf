08828874ea5729e0f37bc87eaf25afee76cea0f4
Felipe Machado <felipelm123@gmail.com>
2018-05-18 11:27:23 -0300

    Deployment by git tag (#110)
    * getting version from tag
    
    * adding coveralls
    
    * removing coveralls
    
    * adding deploy to production step

--- start.sh
+++ start.sh
@@ -17,7 +17,11 @@ fi
 if [ $1 = "prod" ]; then
   cd frontend
   sed -i '/REACT_APP_VERSION/d' .env
-  echo "REACT_APP_VERSION=$(git rev-parse HEAD)" >> .env
+  if git describe --exact-match --tags $(git log -n1 --pretty='%h') ; then
+      echo "REACT_APP_VERSION=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))"
+  else
+      echo "REACT_APP_VERSION=$(git rev-parse HEAD)"
+  fi
   cd ..
   docker-compose -f frontend/docker-compose.yml up -d
 	docker-compose -f backend/docker-compose.yml up -d
