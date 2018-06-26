3015a66d2d0e57ac2aea741cb8b31b49abbdc904
Felipe Machado <felipelm123@gmail.com>
2018-06-26 07:29:15 -0700

    Using current host user uid and group in docker container (#163)

--- start.sh
+++ start.sh
@@ -1,30 +1,31 @@
 
 #!/bin/sh
 if [ -z "$1" ]; then
-  docker-compose -f frontend/docker-compose.yml up -d
-  docker-compose -f backend/docker-compose.yml up
+  CURRENT_UID=$(id -u):$(id -g) docker-compose -f frontend/docker-compose.yml up -d
+  CURRENT_UID=$(id -u):$(id -g) docker-compose -f backend/docker-compose.yml up
   exit 1
 fi
 
 if [ $1 = "backend" ]; then
-	docker-compose -f backend/docker-compose.yml up
+	CURRENT_UID=$(id -u):$(id -g) docker-compose -f backend/docker-compose.yml up
 else 
   if [ $1 = "frontend" ]; then
-  	docker-compose -f frontend/docker-compose.yml up
+    CURRENT_UID=$(id -u):$(id -g) docker-compose -f frontend/docker-compose.yml up
   fi
 fi
 
 if [ $1 = "prod" ]; then
   cd frontend
-  sed -i '/REACT_APP_VERSION/d' .env
+  printf '%q\n' $(awk '!/REACT_APP_VERSION/' .env) > .env
   if git describe --exact-match --tags $(git log -n1 --pretty='%h') ; then
       echo "REACT_APP_VERSION=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))" >> .env
   else
       echo "REACT_APP_VERSION=$(git log --pretty=format:"%h %ai" -1)" >> .env
   fi
   cd ..
-  docker-compose -f frontend/docker-compose.yml up -d
-	docker-compose -f backend/docker-compose.yml up -d
+  CURRENT_UID=$(id -u):$(id -g) docker-compose -f frontend/docker-compose.yml run --entrypoint="yarn build" frontend
+  CURRENT_UID=$(id -u):$(id -g) docker-compose -f frontend/docker-compose.yml up -d
+	CURRENT_UID=$(id -u):$(id -g) docker-compose -f backend/docker-compose.yml up -d
 fi
 
 if [ $1 = "bokeh" ]; then
